15f1ab8dcfb796d8d4ea374f0192a758d079af13
yongfa <853740685@qq.com>
2025-08-26 13:07:19 +0800

    fix 兼容多个数据连接

--- app/settings/config.py
+++ app/settings/config.py
@@ -23,66 +23,69 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "3488a63e1765035d386f05409663f55c83bfae3b3c61a932744b20ad14244dcf"  # openssl rand -hex 32
     JWT_ALGORITHM: str = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 day
+    DB_TYPE: str = os.getenv("DB_TYPE", "sqlite")
+    DB_CONNECTIONS: dict = {
+        "sqlite": {
+            "engine": "tortoise.backends.sqlite",
+            "credentials": {"file_path": f"{BASE_DIR}/db.sqlite3"},  # Path to SQLite database file
+        },
+        # MySQL/MariaDB configuration
+        # Install with: tortoise-orm[asyncmy]
+        "mysql": {
+            "engine": "tortoise.backends.mysql",
+            "credentials": {
+                "host": os.getenv("DB_HOST", "10.1.20.9"),  # Database host address
+                "port": os.getenv("DB_PORT", 3306),  # Database port
+                "user": os.getenv("DB_USER", "root"),  # Database username
+                "password": os.getenv("DB_PASSWORD", "mysql_fxQZsy"),  # Database password
+                "database": os.getenv("DB_NAME", "fastapi-admin"),  # Database name
+            },
+        },
+        # PostgreSQL configuration
+        # Install with: tortoise-orm[asyncpg]
+        # "postgres": {
+        #     "engine": "tortoise.backends.asyncpg",
+        #     "credentials": {
+        #         "host": "localhost",  # Database host address
+        #         "port": 5432,  # Database port
+        #         "user": "yourusername",  # Database username
+        #         "password": "yourpassword",  # Database password
+        #         "database": "yourdatabase",  # Database name
+        #     },
+        # },
+        # MSSQL/Oracle configuration
+        # Install with: tortoise-orm[asyncodbc]
+        # "oracle": {
+        #     "engine": "tortoise.backends.asyncodbc",
+        #     "credentials": {
+        #         "host": "localhost",  # Database host address
+        #         "port": 1433,  # Database port
+        #         "user": "yourusername",  # Database username
+        #         "password": "yourpassword",  # Database password
+        #         "database": "yourdatabase",  # Database name
+        #     },
+        # },
+        # SQLServer configuration
+        # Install with: tortoise-orm[asyncodbc]
+        # "sqlserver": {
+        #     "engine": "tortoise.backends.asyncodbc",
+        #     "credentials": {
+        #         "host": "localhost",  # Database host address
+        #         "port": 1433,  # Database port
+        #         "user": "yourusername",  # Database username
+        #         "password": "yourpassword",  # Database password
+        #         "database": "yourdatabase",  # Database name
+        #     },
+        # },
+    }
     TORTOISE_ORM: dict = {
         "connections": {
-            # SQLite configuration
-            "sqlite": {
-                "engine": "tortoise.backends.sqlite",
-                "credentials": {"file_path": f"{BASE_DIR}/db.sqlite3"},  # Path to SQLite database file
-            },
-            # MySQL/MariaDB configuration
-            # Install with: tortoise-orm[asyncmy]
-            # "mysql": {
-            #     "engine": "tortoise.backends.mysql",
-            #     "credentials": {
-            #         "host": "localhost",  # Database host address
-            #         "port": 3306,  # Database port
-            #         "user": "yourusername",  # Database username
-            #         "password": "yourpassword",  # Database password
-            #         "database": "yourdatabase",  # Database name
-            #     },
-            # },
-            # PostgreSQL configuration
-            # Install with: tortoise-orm[asyncpg]
-            # "postgres": {
-            #     "engine": "tortoise.backends.asyncpg",
-            #     "credentials": {
-            #         "host": "localhost",  # Database host address
-            #         "port": 5432,  # Database port
-            #         "user": "yourusername",  # Database username
-            #         "password": "yourpassword",  # Database password
-            #         "database": "yourdatabase",  # Database name
-            #     },
-            # },
-            # MSSQL/Oracle configuration
-            # Install with: tortoise-orm[asyncodbc]
-            # "oracle": {
-            #     "engine": "tortoise.backends.asyncodbc",
-            #     "credentials": {
-            #         "host": "localhost",  # Database host address
-            #         "port": 1433,  # Database port
-            #         "user": "yourusername",  # Database username
-            #         "password": "yourpassword",  # Database password
-            #         "database": "yourdatabase",  # Database name
-            #     },
-            # },
-            # SQLServer configuration
-            # Install with: tortoise-orm[asyncodbc]
-            # "sqlserver": {
-            #     "engine": "tortoise.backends.asyncodbc",
-            #     "credentials": {
-            #         "host": "localhost",  # Database host address
-            #         "port": 1433,  # Database port
-            #         "user": "yourusername",  # Database username
-            #         "password": "yourpassword",  # Database password
-            #         "database": "yourdatabase",  # Database name
-            #     },
-            # },
+            "default": DB_CONNECTIONS[DB_TYPE],
         },
         "apps": {
             "models": {
                 "models": ["app.models", "aerich.models"],
-                "default_connection": "sqlite",
+                "default_connection": "default",
             },
         },
         "use_tz": False,  # Whether to use timezone-aware datetimes
